58375ad91797b7c0229dd5e40124ad7775211fb8
Sylvia Waldron <sylvia@sylint.dev>
2023-03-06 17:57:51 -0600

    Use timer to delay, RTT to print

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit::{board::Board, hal::prelude::*};
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
+use microbit::{board::Board, hal::{timer::Timer, prelude::* }};
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
-
-    loop {}
+    loop {
+        timer.delay_ms(1000u16);
+        rprintln!("1000 ms passed")
+    }
 }
