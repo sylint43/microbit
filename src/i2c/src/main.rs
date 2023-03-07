7f6f01b08e623232d724b58e5b42a791f4924202
Sylint <sylvia@sylint.dev>
2023-03-07 16:16:04 -0600

    Read command from serial, print data from sensors

--- src/i2c/src/main.rs
+++ src/i2c/src/main.rs
@@ -1,27 +1,92 @@
 #![no_std]
 #![no_main]
 
+use core::{fmt::Write, str::from_utf8};
+use cortex_m::prelude::{_embedded_hal_serial_Read, _embedded_hal_serial_Write};
 use cortex_m_rt::entry;
-use lsm303agr::{AccelOutputDataRate, Lsm303agr};
-use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A, Board};
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use heapless::Vec;
+use lsm303agr::{AccelOutputDataRate, Lsm303agr, MagOutputDataRate};
+use microbit::{
+    hal::{
+        twim,
+        uarte::{self, Baudrate, Parity},
+    },
+    pac::twim0::frequency::FREQUENCY_A,
+    Board,
+};
+use panic_halt as _;
+use serial_setup::UartePort;
+
+mod serial_setup;
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-
     let board = Board::take().unwrap();
     let mut i2c = twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100);
 
     let mut sensor = Lsm303agr::new_with_i2c(i2c);
     sensor.init().unwrap();
     sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    sensor.set_mag_odr(MagOutputDataRate::Hz50).unwrap();
+    let mut sensor = sensor.into_mag_continuous().ok().unwrap();
+
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+
+    let mut buffer: Vec<u8, 32> = Vec::new();
 
     loop {
-        if sensor.accel_status().unwrap().xyz_new_data {
-            let data = sensor.accel_data().unwrap();
-            rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+        buffer.clear();
+
+        loop {
+            let byte = nb::block!(serial.read()).unwrap();
+            // echo byte
+            nb::block!(serial.write(byte)).unwrap();
+            nb::block!(serial.flush()).unwrap();
+
+            if buffer.push(byte).is_err() {
+                write!(serial, "Error: buffer is full.\r\n").unwrap();
+                break;
+            }
+
+            if byte == 13 {
+                break;
+            }
+        }
+
+        let command = from_utf8(&buffer).unwrap();
+
+        match command.trim() {
+            "accelerometer" => {
+                while !sensor.accel_status().unwrap().xyz_new_data {}
+                let data = sensor.accel_data().unwrap();
+                write!(
+                    serial,
+                    "Accelerometer: x {} y {} z{}\r\n",
+                    data.x, data.y, data.z
+                )
+                .unwrap();
+            }
+            "magnetometer" => {
+                while !sensor.mag_status().unwrap().xyz_new_data {}
+                let data = sensor.mag_data().unwrap();
+                write!(
+                    serial,
+                    "Magnetometer: x {} y {} z {}\r\n",
+                    data.x, data.y, data.z
+                )
+                .unwrap();
+            }
+            _ => {
+                write!(serial, "Unknown command: {}\r\n", command).unwrap();
+            }
         }
     }
 }
