9e28f6af2c7a2afdea4c27f8215cc7d267032266
Sylvia Waldron <sylvia@sylint.dev>
2023-03-06 18:00:44 -0600

    Blink LED

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,15 @@ fn main() -> ! {
     let mut board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
 
+    board.display_pins.col1.set_low().unwrap();
+    let mut row1 = board.display_pins.row1;
+
     loop {
+        row1.set_low().unwrap();
+        rprintln!("Dark!");
         timer.delay_ms(1000u16);
-        rprintln!("1000 ms passed")
+        row1.set_high().unwrap();
+        rprintln!("Light!");
+        timer.delay_ms(1000u16);
     }
 }
