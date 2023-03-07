ca36eb1b3957771dd05028ab3da2fecbb46e9c33
Sylint <sylvia@sylint.dev>
2023-03-07 15:23:38 -0600

    Reverse strings

--- src/uart/src/main.rs
+++ src/uart/src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
 use cortex_m_rt::entry;
+use heapless::Vec;
 use microbit::{
     hal::{
         prelude::*,
@@ -27,9 +29,27 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
+    let mut buffer: Vec<u8, 32> = Vec::new();
+
     loop {
-        let byte = nb::block!(serial.read()).unwrap();
-        nb::block!(serial.write(byte)).unwrap();
+        buffer.clear();
+
+        loop {
+            let byte = nb::block!(serial.read()).unwrap();
+
+            if buffer.push(byte).is_err() {
+                write!(serial, "Error: buffer is full\r\n").unwrap();
+                break;
+            }
+
+            if byte == 13 {
+                for byte in buffer.iter().rev().chain(&[b'\n', b'\r']) {
+                    nb::block!(serial.write(*byte)).unwrap();
+                }
+                break;
+            }
+        }
+
         nb::block!(serial.flush()).unwrap();
     }
 }
