c6bd28c10c06d9f9fc88ec07b8ae9a2f9939a200
Sylint <sylvia@sylint.dev>
2023-03-07 15:54:28 -0600

    Read from acc

--- src/i2c/src/main.rs
+++ src/i2c/src/main.rs
@@ -2,16 +2,26 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use microbit::{
-    hal::{prelude::*, twim},
-    pac::twim0::frequency::FREQUENCY_A,
-    Board,
-};
-use panic_halt as _;
+use lsm303agr::{AccelOutputDataRate, Lsm303agr};
+use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A, Board};
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    let board = Board::take().unwrap();
+    rtt_init_print!();
 
-    loop {}
+    let board = Board::take().unwrap();
+    let mut i2c = twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100);
+
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+
+    loop {
+        if sensor.accel_status().unwrap().xyz_new_data {
+            let data = sensor.accel_data().unwrap();
+            rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+        }
+    }
 }
